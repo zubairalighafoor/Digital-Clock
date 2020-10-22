641b1144fccf08d5fdcc6b239784f25206529684
zubair <55566847+zubairhunjrah6004@users.noreply.github.com>
2020-10-22 09:08:48 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,15 @@ import './App.css';
 
 const App=()=> {
   let tim=new Date().toLocaleTimeString();
-  let dat=new Date().toLocaleDateString();
   const [ctime,setCTIME]= useState(tim);
-  const [cDated,setDated]= useState(dat);
   const Update=()=>{
     tim=new Date().toLocaleTimeString();
     setCTIME(tim);
-    dat=new Date().toLocaleDateString();
-    setDated(dat);
   }
   setInterval(Update);
   return (
     <>
      <h1>{ctime}</h1>
-     <h1>{cDated}</h1>
     </>
   )
 }
